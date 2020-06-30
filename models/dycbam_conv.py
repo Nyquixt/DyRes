949c17de9cc47e140523eff1de35adc86ac6d847
Nyquixt <kien.x.ng@gmail.com>
2020-06-30 17:29:44 -0500

    ideas for project

--- models/dycbam_conv.py
+++ models/dycbam_conv.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn 
 
-__all__ = ['DyCBAMConv', 'DyCBAMConv_2', 'DyCBAMConv_3', 'DyCBAMConv_4']
+__all__ = ['DyCBAMConv', 'DyCBAMConv_2', 'DyCBAMConv_3', 'DyCBAMConv_4', 'DyCBAMConv_5']
 
 '''
 [avg, max]
@@ -158,6 +158,59 @@ class DyCBAMConv_4(nn.Module):
         weights_avg = self.attention(gap)
         weights_max = self.attention(mp)
         weights = self.softmax(weights_avg + weights_max).unsqueeze(dim=-1).unsqueeze(dim=-1).unsqueeze(dim=-1)
+        out = weights * all_out
+        out = out.sum(dim=1, keepdim=False)
+        
+        return out
+
+class DyCBAMConv_5(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, bias=False):
+        super(DyCBAMConv_5, self).__init__()
+
+        self.one_conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=bias)
+        self.two_conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=bias)
+        self.three_conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=bias)
+        self.attention = nn.Sequential(
+            nn.Linear(in_channels, 64),
+            nn.ReLU(inplace=True),
+            nn.Linear(64, 3),
+        )
+
+        if in_channels < 16:
+            self.channel_attention = nn.Sequential(
+                nn.Linear(in_channels, 1),
+                nn.ReLU(inplace=True),
+                nn.Linear(1, in_channels),
+                nn.Sigmoid()
+            )
+        else:
+            self.channel_attention = nn.Sequential(
+                nn.Linear(in_channels, in_channels // 16),
+                nn.ReLU(inplace=True),
+                nn.Linear(in_channels // 16, in_channels),
+                nn.Sigmoid()
+            )
+
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        gap = self.avg_pool(x).squeeze(dim=-1).squeeze(dim=-1)
+        mp = self.max_pool(x).squeeze(dim=-1).squeeze(dim=-1)
+        channel = gap * mp
+        channel = self.channel_attention(channel)
+
+        x = x * channel.unsqueeze(dim=-1).unsqueeze(dim=-1).expand_as(x)
+
+        one_out = self.one_conv(x).unsqueeze(dim=1)
+        two_out = self.two_conv(x).unsqueeze(dim=1)
+        three_out = self.three_conv(x).unsqueeze(dim=1)
+        all_out = torch.cat([one_out, two_out, three_out], dim=1)
+        
+        weights_avg = self.attention(gap)
+        weights_max = self.attention(mp)
+        weights = self.sigmoid(weights_avg + weights_max).unsqueeze(dim=-1).unsqueeze(dim=-1).unsqueeze(dim=-1)
         out = weights * all_out
         out = out.sum(dim=1, keepdim=False)
         
