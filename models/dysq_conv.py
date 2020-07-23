11d04ad70e963590808707bb1349b4e6f95e341d
Nyquixt <kien.x.ng@gmail.com>
2020-07-23 18:11:04 -0500

    fix reshaping dysq

--- models/dysq_conv.py
+++ models/dysq_conv.py
@@ -11,8 +11,8 @@ class DySQConv(nn.Module):
         self.gap = nn.AdaptiveAvgPool2d(1) # Global Average Pool
 
         squeeze_channels = in_channels // reduction if in_channels > reduction else 1
-        # groups = squeeze_channels // 4 if squeeze_channels > 4 else 1
-        groups = 1
+        groups = squeeze_channels // 4 if squeeze_channels > 4 else 1
+        # groups = 1
 
         self.squeeze = nn.Conv2d(in_channels, squeeze_channels, kernel_size=1, stride=stride,
                                 groups=groups, bias=bias)
@@ -34,12 +34,12 @@ class DySQConv(nn.Module):
         attention = F.relu(self.squeeze(attention))
         attention = self.excite(attention)
         attention = self.softmax(attention.squeeze(dim=-1).squeeze(dim=-1)).unsqueeze(dim=-1).unsqueeze(dim=-1)
-        attention = attention.view(3, b, -1, 1, 1)
-        x1 = x * attention[0]
+        attention = attention.view(b, 3, -1, 1, 1).permute(1, 0, 2, 3, 4)
+        x1 = x * attention[0].expand_as(x)
         y1 = self.one_bn(self.one_conv(x1))
-        x2 = x * attention[1]
+        x2 = x * attention[1].expand_as(x)
         y2 = self.two_bn(self.two_conv(x2))
-        x3 = x * attention[2]
+        x3 = x * attention[2].expand_as(x)
         y3 = self.three_bn(self.three_conv(x3))
         return y1 + y2 + y3
 
