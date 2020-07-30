53d1ea300f5508097be65ef39859fa251ec50a42
Nyquixt <kien.x.ng@gmail.com>
2020-07-30 18:52:47 -0500

    fix mobile net v2

--- models/cc_mobilenetv2.py
+++ models/cc_mobilenetv2.py
@@ -51,10 +51,10 @@ class CC_MobileNetV2(nn.Module):
     def __init__(self, num_classes=10):
         super(CC_MobileNetV2, self).__init__()
         # NOTE: change conv1 stride 2 -> 1 for CIFAR10
-        self.conv1 = CondConv(3, 32, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(32)
         self.layers = self._make_layers(in_planes=32)
-        self.conv2 = CondConv(320, 1280, kernel_size=1, stride=1, padding=0, bias=False)
+        self.conv2 = nn.Conv2d(320, 1280, kernel_size=1, stride=1, padding=0, bias=False)
         self.bn2 = nn.BatchNorm2d(1280)
         self.linear = nn.Linear(1280, num_classes)
 
