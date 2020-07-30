1881e500cf416b80109b3cc4514a5ca1d72056d0
Nyquixt <kien.x.ng@gmail.com>
2020-07-30 02:55:46 -0500

    minor fix wn

--- models/weightnet.py
+++ models/weightnet.py
@@ -23,7 +23,7 @@ class WeightNet(nn.Module):
         self.fc2 = nn.Conv2d(self.M * out_channels, out_channels * in_channels * kernel_size * kernel_size, 1, 1, 0, groups=self.G * out_channels)
 
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.reduce = nn.Conv2d(in_channels, input_gap, 1, 1, 0, bias=False)
+        self.reduce = nn.Conv2d(in_channels, input_gap, 1, 1, 0, bias=True)
 
     def forward(self, x):
         b, _, _, _ = x.size()
@@ -41,24 +41,24 @@ class WeightNet(nn.Module):
         return x
 
 class WeightNet_DW(nn.Module):
-    def __init__(self, in_channels, kernel_size, stride=1, reduction_ratio=16, M=2, G=2):
+    def __init__(self, channels, kernel_size, stride=1, reduction_ratio=16, M=2, G=2):
         super().__init__()
 
         self.M = M
         self.G = G # lambda = M // G
 
         self.padding = kernel_size // 2
-        input_gap = max(reduction_ratio, in_channels // reduction_ratio)
-        self.in_channels = in_channels
+        input_gap = max(reduction_ratio, channels // reduction_ratio)
+        self.channels = channels
         self.kernel_size = kernel_size
         self.stride = stride
 
-        self.fc1 = nn.Conv2d(input_gap, self.M // self.G * in_channels, 1, 1, 0, groups=1, bias=False)
+        self.fc1 = nn.Conv2d(input_gap, self.M // self.G * channels, 1, 1, 0, groups=1, bias=False)
         self.sigmoid = nn.Sigmoid()
-        self.fc2 = nn.Conv2d(self.M // self.G * in_channels, in_channels * kernel_size * kernel_size, 1, 1, 0, groups=in_channels)
+        self.fc2 = nn.Conv2d(self.M // self.G * channels, channels * kernel_size * kernel_size, 1, 1, 0, groups=channels)
 
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.reduce = nn.Conv2d(in_channels, input_gap, 1, 1, 0, bias=False)
+        self.reduce = nn.Conv2d(channels, input_gap, 1, 1, 0, bias=False)
 
     def forward(self, x):
         b, _, _, _ = x.size()
@@ -71,8 +71,8 @@ class WeightNet_DW(nn.Module):
 
         x = x.view(1, -1, x.size(2), x.size(3))
         x_w = x_w.view(-1, 1, self.kernel_size, self.kernel_size)
-        x = F.conv2d(x, weight=x_w, stride=self.stride, padding=self.padding, groups=b * self.in_channels)
-        x = x.view(-1, self.in_channels, x.size(2), x.size(3))
+        x = F.conv2d(x, weight=x_w, stride=self.stride, padding=self.padding, groups=b * self.channels)
+        x = x.view(-1, self.channels, x.size(2), x.size(3))
         return x
 
 def test():
