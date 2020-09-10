10a0d925c7becb8c3901ff9f212f1b2265f765b7
Nyquixt <kien.x.ng@gmail.com>
2020-09-10 02:02:59 -0500

    small bug imagenet model

--- imagenet/ms_alexnet.py
+++ imagenet/ms_alexnet.py
@@ -7,40 +7,38 @@ __all__ = ['MS_AlexNet']
 
 class MS_AlexNet(nn.Module):
 
-    def __init__(self, num_classes=10, input_size=32):
+    def __init__(self, num_classes=1000, input_size=32):
         super(MS_AlexNet, self).__init__()
         self.features = nn.Sequential(
-            MSConv(3, 64, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(64),
+            MSConv(3, 64, kernel_size=11, stride=4, padding=2),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2),
-            MSConv(64, 192, kernel_size=3, padding=1),
-            nn.BatchNorm2d(192),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            MSConv(64, 192, kernel_size=5, padding=2),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2),
+            nn.MaxPool2d(kernel_size=3, stride=2),
             MSConv(192, 384, kernel_size=3, padding=1),
-            nn.BatchNorm2d(384),
             nn.ReLU(inplace=True),
             MSConv(384, 256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
             MSConv(256, 256, kernel_size=3, padding=1),
-            nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2),
+            nn.MaxPool2d(kernel_size=3, stride=2)
         )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
         self.classifier = nn.Sequential(
             nn.Dropout(),
-            nn.Linear(256 * 2 * 2 if input_size==32 else 256 * 4 * 4, 4096),
+            nn.Linear(256 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
             nn.Dropout(),
-            nn.Linear(4096, 1024 if num_classes == 10 else 4096),
+            nn.Linear(4096, 4096),
             nn.ReLU(inplace=True),
-            nn.Linear(1024 if num_classes == 10 else 4096, num_classes),
+            nn.Linear(4096, num_classes)
         )
+
 
     def forward(self, x):
         x = self.features(x)
+        x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.classifier(x)
         return x
