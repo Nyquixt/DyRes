2c9b0e802cec8526428440eb4b7b4788f4cc9ffd
Nyquixt <kien.x.ng@gmail.com>
2020-09-28 20:58:21 -0500

    include gc_att in gcwn

--- convs/gc_weightnet.py
+++ convs/gc_weightnet.py
@@ -1,9 +1,38 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from convs.gcnet import GC_Attention
 
 __all__ = ['GC_WeightNet']
+
+class GC_Attention(nn.Module):
+    def __init__(self, in_channels, out_channels, reduction=16):
+        super().__init__()
+        reduction_channels = max(in_channels // reduction, reduction)
+        self.conv_mask = nn.Conv2d(in_channels, 1, kernel_size=1) #K
+        
+        self.transform = nn.Sequential(
+            nn.Conv2d(in_channels, reduction_channels, kernel_size=1),
+            nn.LayerNorm([reduction_channels, 1, 1]),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(reduction_channels, out_channels, kernel_size=1),
+        )
+
+        self.softmax = nn.Softmax(dim=2)
+
+    def forward(self, x):
+        b, c_in, h, w = x.size()
+        # Context Modeling
+        input_x = x
+        input_x = input_x.view(b, c_in, h * w).unsqueeze(1) # N x 1 x C_in x H*W
+        context_mask = self.conv_mask(x) # N x C_in x H x W
+        context_mask = context_mask.view(b, 1, h * w) # N x 1 x H*W
+        context_mask = self.softmax(context_mask)
+        context_mask = context_mask.unsqueeze(-1) # N x 1 x H*W x 1
+        context = torch.matmul(input_x, context_mask)
+        context = context.view(b, c_in, 1, 1)
+        # Transform
+        out = self.transform(context)
+        return out
 
 class GC_WeightNet(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, reduction=16, M=2, G=2):
