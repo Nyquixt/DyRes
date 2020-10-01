f83c24ea4cfdd3cbefc5bb0d169492f0a589c7e3
Nyquixt <kien.x.ng@gmail.com>
2020-10-01 16:34:11 -0500

    remove previous exps

--- cifar/__init__.py
+++ cifar/__init__.py
@@ -9,10 +9,7 @@ from .cc_alexnet import *
 from .dy_alexnet import *
 from .wn_alexnet import *
 # Experiment
-from .dyres_alexnet import *
-from .dysep_alexnet import *
 from .gcwn_alexnet import *
-from .nlc_alexnet import *
 from .nlcwn_alexnet import *
 from .dnlcwn_alexnet import *
 
@@ -22,10 +19,7 @@ from .cc_resnet import *
 from .dy_resnet import *
 from .wn_resnet import *
 # Experiment
-from .dyres_resnet import *
-from .dysep_resnet import *
 from .gcwn_resnet import *
-from .nlc_resnet import *
 from .nlcwn_resnet import *
 from .dnlcwn_resnet import *
 
@@ -35,9 +29,6 @@ from .cc_mobilenetv2 import *
 from .dy_mobilenetv2 import *
 from .wn_mobilenetv2 import *
 # Experiment
-from .dyres_mobilenetv2 import *
-from .dysep_mobilenetv2 import *
 from .gcwn_mobilenetv2 import *
-from .nlc_mobilenetv2 import *
 from .nlcwn_mobilenetv2 import *
 from .dnlcwn_mobilenetv2 import *
