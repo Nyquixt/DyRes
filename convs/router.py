03a49fbcd44a9221251ad0763669cf06dc679e17
Kien Nguyen <kien.x.ng@gmail.com>
2020-10-30 00:29:20 -0500

    update router

--- convs/router.py
+++ convs/router.py
@@ -2,9 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import math
-class Router(nn.Module):
-    def __init__(self, in_channels, num_experts, reduction=16, pool_size =4):
-        super(Router, self).__init__()
+class route_func(nn.Module):
+    def __init__(self, in_channels, num_experts, reduction=16, pool_size=4):
+        super().__init__()
         reduction_channels = max(in_channels // reduction, reduction)
         self.channel_extractor = nn.Sequential(
             nn.Linear(in_channels,reduction_channels,bias=False),
@@ -28,9 +28,42 @@ class Router(nn.Module):
 
         return general
 
+class RouterConv(nn.Module):
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, groups=1, num_experts=3, reduction=16, spatial=4):
+        super().__init__()
+
+        self.stride = stride
+        self.padding = padding
+        self.groups = groups
+
+        # routing function
+        self.routing_func = route_func(in_channels, num_experts, reduction, spatial)
+
+        self.weight = nn.Parameter(torch.Tensor(num_experts, out_channels, in_channels // groups, kernel_size, kernel_size))
+        
+        self.register_parameter('bias', None)
+        nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
+
+    def forward(self, x):
+        routing_weight = self.routing_func(x) # N x k x C_in
+        routing_weight = routing_weight.unsqueeze(dim=2).unsqueeze(dim=-1).unsqueeze(dim=-1) # N x k x 1 x C_in x 1 x 1
+
+        b, c_in, h, w = x.size()
+        k, c_out, c_in, kh, kw = self.weight.size()
+        x = x.view(1, -1, h, w) # 1 x N*C_in x H x W
+        weight = self.weight.unsqueeze(dim=0) # 1 x k x C_out x C_in x kH x hW 
+
+        combined_weight = (routing_weight * weight).sum(1).view(-1, c_in, kh, kw)
+        output = F.conv2d(x, weight=combined_weight, bias=None, 
+                            stride=self.stride, padding=self.padding, groups=self.groups * b)
+
+        output = output.view(b, c_out, output.size(-2), output.size(-1))
+        return output
+
 def demo():
-    net = Router(3,3)
+    net = RouterConv(3, 16, 3)
     x = torch.randn(1,3,32,32)
     y = net(x)
     print(y.shape)
-demo()
+# demo()
