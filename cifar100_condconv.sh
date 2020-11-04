0a6054703175a7ffe2f28671e876449d413bd3b6
Kien Nguyen <kien.x.ng@gmail.com>
2020-11-04 12:02:16 -0600

    delete training scripts

--- cifar100_condconv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-CUDA_VISIBLE_DEVICES=0,1 nohup python3 train.py -n cc_alexnet --dataset cifar100 --ngpu 2 --save --cuda &
-CUDA_VISIBLE_DEVICES=2,3,4 nohup python3 train.py -n cc_resnet18 --dataset cifar100 --ngpu 3 --save --cuda &
-CUDA_VISIBLE_DEVICES=5,6,7 nohup python3 train.py -n cc_mobilenetv2 --dataset cifar100 --ngpu 3 --save --cuda &
--- imagenet_resnet18.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 nohup python3 train.py -n resnet18 --dataset imagenet --ngpu 8 --save --cuda &
